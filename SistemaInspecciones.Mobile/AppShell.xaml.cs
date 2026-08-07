5d10d1a23ea8dce71795989e30949413c31b190b
EnriqueMatute777 <139660721+ineedcode777@users.noreply.github.com>
2026-08-07 15:30:51 -0600

    Estructura inicial: 5 proyectos Clean Architecture + MAUI

---
+++ SistemaInspecciones.Mobile/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace SistemaInspecciones.Mobile
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
